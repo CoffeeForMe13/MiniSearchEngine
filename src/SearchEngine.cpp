3f9081eff3da8ba2cc61d6bb5fc4544a50611135
Adrian-Robert Marin <adymarin013@gmail.com>
2025-11-27 13:27:36 +0200

    buildIndex method implemented

--- src/SearchEngine.cpp
+++ src/SearchEngine.cpp
@@ -6,6 +6,17 @@ SearchEngine::SearchEngine(FileScanner &scanner, Indexer &indexer)
 {
 }
 
+void SearchEngine::buildIndex()
+{
+    auto files = scanner_.listTextFiles();
+
+    for (const auto& file : files)
+    {
+        std::string content = scanner_.readFile(file);
+        indexer_.addDocument(file, content);
+    }
+}
+
 SearchEngine::~SearchEngine()
 {
 }
