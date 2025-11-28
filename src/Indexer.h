dc09869a6d332f46b60950132c5774ce930fddcc
Adrian-Robert Marin <adymarin013@gmail.com>
2025-11-28 18:08:41 +0200

    Add a positional index that records word order

--- src/Indexer.h
+++ src/Indexer.h
@@ -8,12 +8,15 @@ class Indexer
 {
 public:
 	using InvertedIndex = std::unordered_map<std::string, std::vector<std::string>>;
+	using PositionalIndex = std::unordered_map<std::string, std::unordered_map<std::string, std::vector<int>>>;
 
 	void addDocument(const std::string& filePath, const std::string& content);
 	const InvertedIndex& getIndex() const;
+	const PositionalIndex& getPositionalIndex() const;
 
 private:
 	InvertedIndex index_;
+	PositionalIndex positionalIndex_;
 
 	std::vector<std::string> tokenize(const std::string& text) const;
 
